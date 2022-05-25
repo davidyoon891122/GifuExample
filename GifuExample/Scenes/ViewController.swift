06c8d521972f55be9f2736c9fc1593b5e8e87c04
Yoonjiwon <k112751@koreainvestment.com>
2022-05-25 10:09:24 +0900

    apply gifu

--- GifuExample/Scenes/ViewController.swift
+++ GifuExample/Scenes/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import Gifu
+import SnapKit
 class ViewController: UIViewController {
-
+    private lazy var gifImageView: GIFImageView = {
+        let imageView = GIFImageView()
+        imageView.animate(withGIFNamed: "giphy")
+        imageView.layer.cornerRadius = 75
+        imageView.layer.masksToBounds = true
+        return imageView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .systemBackground
+        setupViews()
     }
-
-
 }
 
+private extension ViewController {
+    func setupViews() {
+        [
+            gifImageView
+        ]
+            .forEach {
+                view.addSubview($0)
+            }
+
+        gifImageView.snp.makeConstraints {
+            $0.width.height.equalTo(150.0)
+            $0.centerY.equalToSuperview()
+            $0.centerX.equalToSuperview()
+        }
+    }
+}
